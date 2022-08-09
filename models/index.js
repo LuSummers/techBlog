625f433f31bb4166666ea58c7d55c9867e9646ee
Lu Summers <lusummers@users.noreply.github.com>
2022-08-09 04:05:00 -0400

    comment model

--- models/index.js
+++ models/index.js
@@ -1,16 +1,22 @@
-const User = require("./User");
-const Post = require("./Post");
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-
-
-// create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-  })
-
-
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+POst.hasMany(Comment,{
+  foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
     foreignKey: 'user_id',
-  });
-module.exports = { User, Post };
+})
+
+module.exports = { User, Post, Comment };
