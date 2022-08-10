bf118835499ddc5b4fdece205159b6ac122b2f02
Lu Summers <lusummers@users.noreply.github.com>
2022-08-10 01:11:59 -0400

    updated js file

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ const hbs = exphbs.create({});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/static", express.static('./static/'));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
